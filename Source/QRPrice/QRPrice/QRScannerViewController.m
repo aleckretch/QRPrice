f603d3d4d0494daa48dd9f14161fded1b6ed860d
Kyle <KyleKnez@kyles-macbook-pro.local>
2016-02-01 23:45:34 -0700

    QR Scanner Implemented

--- Source/QRPrice/QRPrice/QRScannerViewController.m
+++ Source/QRPrice/QRPrice/QRScannerViewController.m
@@ -7,32 +7,113 @@
 //
 
 #import "QRScannerViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface QRScannerViewController ()
-
+@interface QRScannerViewController  () <AVCaptureMetadataOutputObjectsDelegate>
+{
+    AVCaptureSession *session;
+    AVCaptureVideoPreviewLayer *previewLayer;
+    UIView *higlightView;
+    UILabel *label;
+}
 @end
 
 @implementation QRScannerViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blackColor]; //TODO remove this
-    // Do any additional setup after loading the view.
+    
+    label = [[UILabel alloc] init];
+    label.frame = CGRectMake(0, self.view.bounds.size.height - 40, self.view.bounds.size.width, 40);
+    label.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    label.backgroundColor = [UIColor colorWithWhite:0.15 alpha:0.65];
+    label.textColor = [UIColor whiteColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = @"(none)";
+    [self.view addSubview:label];
+    
+    higlightView = [[UIView alloc] init];
+    higlightView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleBottomMargin;;
+    higlightView.layer.borderColor = [[UIColor greenColor] CGColor];
+    higlightView.layer.borderWidth = 3;
+    [self.view addSubview: higlightView];
+    
+    session = [[AVCaptureSession alloc] init];
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *inputError = nil;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&inputError];
+    
+    if (inputError != nil) {
+        NSLog(@"You can't use a simulator because it doesn't have a camera");
+        return;
+    }
+    
+    [session addInput:input];
+    
+    AVCaptureMetadataOutput *output = [[AVCaptureMetadataOutput alloc] init];
+    [output setMetadataObjectsDelegate:self queue: dispatch_get_main_queue()];
+    [session addOutput:output];
+    output.metadataObjectTypes = [output availableMetadataObjectTypes];
+    
+    previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    previewLayer.frame = self.view.bounds;
+    previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [self.view.layer addSublayer:previewLayer];
+    
+    [session startRunning];
+    
+    [self.view bringSubviewToFront:higlightView];
+    [self.view bringSubviewToFront:label];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
+    
+    CGRect highlightViewRect = CGRectZero;
+    AVMetadataObject *barCodeObject;
+    NSString *detectionString;
+    
+    NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode,
+                             AVMetadataObjectTypeCode39Code,
+                             AVMetadataObjectTypeCode39Mod43Code,
+                             AVMetadataObjectTypeEAN13Code,
+                             AVMetadataObjectTypeEAN8Code,
+                             AVMetadataObjectTypeCode93Code,
+                             AVMetadataObjectTypeCode128Code,
+                             AVMetadataObjectTypePDF417Code,
+                             AVMetadataObjectTypeQRCode,
+       						 AVMetadataObjectTypeAztecCode ];
+    
+    for(AVMetadataObject* metadata in metadataObjects) {
+        for(NSString* objectString in barCodeTypes) {
+            if ([objectString isEqualToString: metadata.type]) {
+                barCodeObject = [ previewLayer transformedMetadataObjectForMetadataObject:metadata];
+                highlightViewRect = barCodeObject.bounds;
+                detectionString = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
+                // TODO: UNComment this if you want it to stop when it finds a bar code.
+                // [session stopRunning];
+                break;
+            }
+        }
+        
+        if (detectionString != nil)
+        {
+            label.text = detectionString;
+            break;
+        } else {
+            label.text = @"(none)";
+        }
+    }
+    
+    higlightView.frame = highlightViewRect;
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
+
+
+
+
+
+
+
